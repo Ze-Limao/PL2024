89964cee1b7c743eae1e3760526a81579e69f938
Ze-Limao <jeswitch.24@gmail.com>
2024-03-12 11:35:00 +0000

    tpc 5

---
+++ TPC5/README.md
@@ -0,0 +1,85 @@
+# TPC5: MÁQUINA DE VENDAS
+#### Autor: José Afonso Lopes Correia, A100610  
+
+## Resumo:
+Programa em Python que simula o funcionamento de uma máquina de vendas. Este programa permite que os utilizadores interajam com a máquina inserindo comandos para listar produtos, adicionar moedas, selecionar produtos e sair do mesmo.
+
+Para tal, o programa utiliza um analisador léxico para interpretação dos diversos comandos:
+
+`listar` - lista os produtos disponíveis
+
+`moeda <lista de moedas>` - adiciona a quantia total inserida ao saldo
+
+`selecionar <id produto>` - prodeece à compra do produto selecionado, caso o saldo seja suficiente
+
+`sair` - calcula o troco e termina o programa
+
+### Manipulação de Dados dos Produtos:
+Os dados dos produtos são fornecidos a partir de um ficheiro JSON, seguindo uma estrutura específica que inclui o ID do produto, o nome e o preço.
+
+## Demonstração:
+```
+Bem-vindo à máquina de vendas do Zé!
+         ____________________________________________
+        |############################################|
+        |#|                           |##############|
+        |#|  =====  ..--''`  |~~``|   |##|````````|##|
+        |#|  |   |  \     |  :    |   |##| Exact  |##|
+        |#|  |___|   /___ |  | ___|   |##| Change |##|
+        |#|  /=__\\  ./.__\\   |/,__\\|##| Only   |##|
+        |#|  \\__//   \\__//    \\__//|##|________|##|
+        |#|===========================|##############|
+        |#|```````````````````````````|##############|
+        |#| =.._      +++     //////  |##############|
+        |#| \\/  \\     | |     \\    |#|`````````|##|
+        |#|  \\___\\    |_|     /___ /|#| _______ |##|
+        |#|  / __\\\\  /|_|\\   // __\|#| |1|2|3| |##|
+        |#|  \\__//-  \\|_//   -\\__//|#| |4|5|6| |##|
+        |#|===========================|#| |7|8|9| |##|
+        |#|```````````````````````````|#| ``````` |##|
+        |#| ..--    ______   .--._.   |#|[=======]|##|
+        |#| \\   \\   |    |   |    | |#|  _   _  |##|
+        |#|  \\___\\  : ___:   | ___| |#| ||| ( ) |##|
+        |#|  / __\\  |/ __\\   // __\\|#| |||  `  |##|
+        |#|  \\__//   \\__//  /_\\__//|#|  ~      |##|
+        |#|===========================|#|_________|##|
+        |#|```````````````````````````|##############|
+        |############################################|
+        |#|||||||||||||||||||||||||||||####```````###|
+        |#||||||||||||PUSH|||||||||||||####\\|||||/##|
+        |############################################|
+        \\\\\\\\\\\\\\\\\\\\\\///////////////////////
+         |_________________________________|CR8|___|
+
+Comandos disponíveis: listar, moeda, selecionar, sair
+
+$ listar
+    Número      |            Nome                              |      Preço
+       1        |        Par de meias                          |       1.8
+       2        |        Cheirinho para o carro                |       1.95
+       3        |        Guarda-Chuva                          |       3.5
+       4        |        Gomas dos Ursinhos                    |       1.65
+       5        |        Bolacha-Joao                          |       0.45
+       6        |        Palmier de Chocolate                  |       0.75
+       7        |        Agua                                  |       0.6
+       8        |        Cola-Cola                             |       0.95
+       9        |        Hyper Bock Mini                       |       1.2
+
+$ moeda 2e, 50c, 1e, 10c
+Saldo: 3.6€
+
+$ selecionar 3
+Produto Guarda-Chuva comprado. Saldo restante: 0.1€
+
+$ sair
+Troco:
+0 x 2€
+0 x 1€
+0 x 0.5€
+0 x 0.2€
+1 x 0.1€
+0 x 0.05€
+Obrigado por usar a máquina de vendas do Zé!
+```
+
+Recorreu-se à biblioteca [ply](https://www.dabeaz.com/ply/ply.html), uma ferramenta para a construção de analisadores léxicos e sintáticos.
---
+++ TPC5/main.py
@@ -0,0 +1,137 @@
+import sys
+import json
+import ply.lex as lex
+
+#Máquina de vendas
+tokens = (
+    "LISTAR",
+    "MOEDA",
+    "SELECIONAR",
+    "SAIR"
+)
+
+def t_LISTAR(t):
+    r'listar'
+    return t
+
+def t_MOEDA(t):
+    r'moeda[ ]([5c|10c|20c|50c|1e|2e],?)+' # Uma ou mais moedas separadas por vírgula
+    return t
+
+def t_SELECIONAR(t):
+    r'selecionar[ ]\d+'
+    return t
+
+def t_SAIR(t):
+    r'sair'
+    return t
+
+
+t_ignore = '\t\n'
+
+def t_error(t):
+    sys.stderr.write(f"Error: Unexpected command {t.value[0]}\n")
+    t.lexer.skip(1) # Ignorar o token
+
+def get_change(saldo):
+    change = {}
+    coins = [2, 1, 0.5, 0.2, 0.1, 0.05]
+    for coin in coins:
+        change[coin] = int(saldo // coin)
+        saldo = round(saldo % coin, 2)
+    return change
+
+
+def main(argv):
+    if (len(argv) < 2):
+        print("Usage: python main.py <input_file>")
+        return
+
+    # Ler o ficheiro json
+    with open(argv[1], "r") as file:
+        data = json.load(file)
+
+    # Criar a tabela de produtos
+    produtos = data["produtos"]
+    tabela = {}
+    for produto in produtos:
+        tabela[produto["id"]] = produto
+    
+
+    lexer = lex.lex() # Criar o lexer
+    saldo = 0 # Saldo do utilizador
+    print("""
+Bem-vindo à máquina de vendas do Zé!
+         ____________________________________________
+        |############################################|
+        |#|                           |##############|
+        |#|  =====  ..--''`  |~~``|   |##|````````|##|
+        |#|  |   |  \     |  :    |   |##| Exact  |##|
+        |#|  |___|   /___ |  | ___|   |##| Change |##|
+        |#|  /=__\\  ./.__\\   |/,__\\   |##| Only   |##|
+        |#|  \\__//   \\__//    \\__//   |##|________|##|
+        |#|===========================|##############|
+        |#|```````````````````````````|##############|
+        |#| =.._      +++     //////  |##############|
+        |#| \\/  \\     | |     \\    \\  |#|`````````|##|
+        |#|  \\___\\    |_|     /___ /  |#| _______ |##|
+        |#|  / __\\\\  /|_|\\   // __\\   |#| |1|2|3| |##|
+        |#|  \\__//-  \\|_//   -\\__//   |#| |4|5|6| |##|
+        |#|===========================|#| |7|8|9| |##|
+        |#|```````````````````````````|#| ``````` |##|
+        |#| ..--    ______   .--._.   |#|[=======]|##|
+        |#| \\   \\   |    |   |    |   |#|  _   _  |##|
+        |#|  \\___\\  : ___:   | ___|   |#| ||| ( ) |##|
+        |#|  / __\\  |/ __\\   // __\\   |#| |||  `  |##|
+        |#|  \\__//   \\__//  /_\\__//   |#|  ~      |##|
+        |#|===========================|#|_________|##|
+        |#|```````````````````````````|##############|
+        |############################################|
+        |#|||||||||||||||||||||||||||||####```````###|
+        |#||||||||||||PUSH|||||||||||||####\\|||||/###|
+        |############################################|
+        \\\\\\\\\\\\\\\\\\\\\\///////////////////////
+         |________________________________| CR8|___|
+""")
+    print("Comandos disponíveis: listar, moeda, selecionar, sair")
+    for line in sys.stdin: # Para cada linha do input
+        lexer.input(line)
+        for token in lexer:
+            if token.type == "LISTAR":
+                print('     Número     |            Nome                              |       Preço')
+                for produto in produtos:
+                    print(f'       {produto["id"]}        |        {produto["nome"]: <30}        |       {produto["preco"]}')
+
+            elif token.type == "MOEDA":
+                moedas = token.value.split(" ")[1].split(",")
+                for moeda in moedas:
+                    if moeda[-1] == "e": # moeda[-1] para remover o "e"
+                        saldo += float(moeda[:-1])
+                    else: 
+                        saldo += float(moeda[:-1]) / 100 # Se for uma moeda de cêntimos
+                print(f"Saldo: {saldo}€")
+            
+            elif token.type == "SELECIONAR":
+                id = int(token.value.split(" ")[1])
+                if id in tabela:
+                    produto = tabela[id]
+                    if saldo >= produto["preco"]:
+                        saldo -= produto["preco"]
+                        print(f"Produto {produto['nome']} comprado. Saldo restante: {saldo}€")
+                    else:
+                        print("Saldo insuficiente")
+                else:
+                    print(f"Produto com id {id} não existe")
+
+            elif token.type == "SAIR":
+                change = get_change(saldo)
+                # Imprimir o troco com asccii art
+                print("Troco:")
+                for coin in change:
+                    print(f"{change[coin]} x {coin}€")
+                print("Obrigado por usar a máquina de vendas do Zé!")
+                return
+
+
+if __name__ == "__main__":
+    main(sys.argv)
